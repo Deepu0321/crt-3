88743acdb886b77c41286bc4f72d362d022deb3e
Deepu0321 <hemalathavallepu2005@gmail.com>
2026-03-10 10:30:48 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+class Factorial {
+
+    
+    static int factorial(int n) {
+        if (n == 1) {        
+            return 1;
+        } else {
+            return n * factorial(n - 1);  
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        int result = factorial(n);
+
+        System.out.println("Factorial of " + n + " is: " + result);
+    }
+}
